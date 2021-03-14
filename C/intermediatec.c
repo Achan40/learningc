01cad34c0b5df94ed059d3c60f398ff3a6559922
Aaron <chanman2841@gmail.com>
2021-03-14 15:15:51 -0500

    started intermediate c

---
+++ C/intermediatec.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*functions*/
+void sayHi(char name[], int age){
+	printf("Hello %s, you are %d\n", name,age);
+}
+
+/*If statements*/
+int max(int num1, int num2, int num3){
+	int result;
+	
+	if(num1 >= num2 && num1 >= num3){
+		result = num1;
+	} else if (num2 >= num1 && num2 >= num3){
+		result = num2;
+	} else {
+		result = num3;
+	}
+	return result;
+}
+
+/*prototyping a function*/
+double cubed(double num);
+
+int main()
+{
+	/*Arrays*/
+	int luckyNumbers[] = {4,8,16,24,32};
+	luckyNumbers[0] = 40;
+	printf("%d\n", luckyNumbers[0]);
+
+	int emptyNums[20];
+	emptyNums[1] = 30;
+	emptyNums[0] = 10;
+	printf("%d\n", emptyNums[0]);
+
+	/*functions*/
+	sayHi("Aaron", 22);
+
+	printf("Cubed function Answer: %f\n", cubed(3.0));
+
+	printf("%d\n", max(4, 10, 15));
+
+	return 0;
+}
+
+/*functions*/
+double cubed(double num){
+	return num * num * num;
+}
+
