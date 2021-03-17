2ba0f2bee6b25fdfa31b28bdaf702dabb42e0ff6
Aaron <chanman2841@gmail.com>
2021-03-16 22:02:18 -0500

    started nested loops

---
+++ C/advancedc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(){
+
+	/*for loops*/
+	int i = 1;
+	while(i <= 5){
+		printf("%d\n", i);
+		i++;
+	}
+
+	int luckyNumbers[] = {4, 8, 12, 16, 15};
+	int j;
+	for(j = 1; j < 5; j++){
+		printf("%d\n", luckyNumbers[j]);
+	}
+
+	/*nested loops and 2D arrays*/
+	int nums[3][2] = {
+		{1,2},
+		{3,4},
+		{5,6}
+	};
+
+	int x,y;
+	for(x = 0; x < 3; x++){
+		for(y = 0; y < 2; y++){
+			printf("%d\n", nums[x][y]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
