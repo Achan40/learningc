b97522d06cb3b85eda7550b7af88980c7f863ef3
Aaron <chanman2841@gmail.com>
2021-07-28 13:33:15 -0500

    finish memoization practice

--- dynamicprogamming/allconstruct.js
+++ dynamicprogamming/allconstruct.js
@@ -1,26 +1,50 @@
 // function should return all ways to create target, a collection of arrays
 // m = target.length
 // n = wordBank.length
-// O(n^m * m) time 
-// O(m^2) space
+// O(n^m) time 
+// O(m) space
 const allConstruct = (target, wordBank) => {
+    if (target === '') return [[]];
 
+    const result = [];
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0){
+            const suffix = target.slice(word.length);
+            const suffixWays = allConstruct(suffix, wordBank);
+            const targetWays = suffixWays.map(way => [word, ...way]);
+            result.push(...targetWays);
+        }
+    }
+    return result;
 }
 
-// memoized
-// O(n * m^2) time complexity
-// O(m^2) space complexity
-const allConstructM = (target, wordBank) => {
+// memoized doesn't do too much here
+const allConstructM = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target];
+    if (target === '') return [[]];
 
+    const result = [];
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0){
+            const suffix = target.slice(word.length);
+            const suffixWays = allConstruct(suffix, wordBank, memo);
+            const targetWays = suffixWays.map(way => [word, ...way]);
+            result.push(...targetWays);
+        }
+    }
+    memo[target] = result;
+    return result;
 }
 
-console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // 1
-console.log(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk","boar"])); // 0
-console.log(allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o","t"])); // 4
-//console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeeeeee", "eeeeeeeee", "eeeeeeeeeeee"])); // 0
+console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); 
+console.log(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk","boar"])); 
+console.log(allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o","t"])); 
+//console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeeeeee", "eeeeeeeee", "eeeeeeeeeeee"])); 
 
 
-console.log(allConstructM("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // 1
-console.log(allConstructM("skateboard", ["bo", "rd", "ate", "t", "ska", "sk","boar"])); // 0
-console.log(allConstructM("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o","t"])); // 4
-console.log(allConstructM("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeeeeee", "eeeeeeeee", "eeeeeeeeeeee"])); // 0
+console.log(allConstructM("abcdef", ["ab", "abc", "cd", "def", "abcd"])); 
+console.log(allConstructM("skateboard", ["bo", "rd", "ate", "t", "ska", "sk","boar"])); 
+console.log(allConstructM("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o","t"])); 
+// console.log(allConstructM("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeeeeee", "eeeeeeeee", "eeeeeeeeeeee"])); 
